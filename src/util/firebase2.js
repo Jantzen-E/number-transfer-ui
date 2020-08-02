f7bcb6588e1ef8b62eaeeaf8e49ff3233e466658
Jantzen-E <jantzen.egan@gmail.com>
2020-08-02 14:27:02 -0600

    comment out 2nd firebase db file to troubleshoot

--- src/util/firebase2.js
+++ src/util/firebase2.js
@@ -1,17 +1,17 @@
-import firebase from 'firebase';
+// import firebase from 'firebase';
 
-const firebaseConfig2 = {
-    apiKey: "AIzaSyBmqHyvoYMCe5jOocCR10AySEuoYqIvJJo",
-    authDomain: "number-transfer2.firebaseapp.com",
-    databaseURL: "https://number-transfer2.firebaseio.com",
-    projectId: "number-transfer2",
-    storageBucket: "number-transfer2.appspot.com",
-    messagingSenderId: "726754897588",
-    appId: "1:726754897588:web:86d8eef36ff36e60ea31e1",
-    measurementId: "G-L8HDWVM8RQ"
-};
+// const firebaseConfig2 = {
+//     apiKey: "AIzaSyBmqHyvoYMCe5jOocCR10AySEuoYqIvJJo",
+//     authDomain: "number-transfer2.firebaseapp.com",
+//     databaseURL: "https://number-transfer2.firebaseio.com",
+//     projectId: "number-transfer2",
+//     storageBucket: "number-transfer2.appspot.com",
+//     messagingSenderId: "726754897588",
+//     appId: "1:726754897588:web:86d8eef36ff36e60ea31e1",
+//     measurementId: "G-L8HDWVM8RQ"
+// };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig2);
+// firebase.initializeApp(firebaseConfig2);
 
-export default firebase;
+// export default firebase;
